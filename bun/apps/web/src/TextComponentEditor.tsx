795336c39ae3cee838bfe4da97f6074785dba306
imakebloatedcode <sometimesmycodeworks@outlook.com>
2026-06-29 09:04:44 -0600

    Fix lack of events firing for non-formatting changes to the text field

--- bun/apps/web/src/TextComponentEditor.tsx
+++ bun/apps/web/src/TextComponentEditor.tsx
@@ -134,6 +134,11 @@ export const TextEditor = ({
             }
           }
         }}
+        onInput={() => {
+          if (onChange && editor.current) {
+            onChange(divToTextComponents(editor.current));
+          }
+        }}
         className="textEditor-obf-resolve text-left border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 field-sizing-content min-h-16 w-full rounded-b-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
       ></div>
     </div>
